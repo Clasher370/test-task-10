277375a43883b6d9b3647ba75d1dc62aabfbe8f2
Clasher370 <tkasha89@gmail.com>
2017-10-09 13:54:54 +0300

    refactor model spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe Post do
   it { should belong_to(:author).class_name('User') }
   it { should have_many(:comments) }
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:body) }
-  it { should validate_presence_of(:published_at) }
+
+  %i[title body published_at].each do |attr|
+    it { should validate_presence_of(attr) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 describe User do
-  it { should have_many(:comments).dependent(:destroy) }
-  it { should have_many(:comments).with_foreign_key('author_id') }
+  %i[posts comments].each do |model|
+    it { should have_many(model).dependent(:destroy) }
+    it { should have_many(model).with_foreign_key('author_id') }
+  end
 
-  it { should have_many(:posts).dependent(:destroy) }
-  it { should have_many(:posts).with_foreign_key('author_id') }
-
-  it { should validate_presence_of(:nickname) }
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password_digest) }
+  %i[nickname email password_digest].each do |attr|
+    it { should validate_presence_of(attr) }
+  end
 
   it { should have_secure_password }
 end
