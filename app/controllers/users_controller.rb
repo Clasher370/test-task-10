97439a28b4aa2ce313c74d4a2e84d3ef60a90f2b
Clasher370 <tkasha89@gmail.com>
2017-10-19 10:57:12 +0300

    users#create

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { message: 'Created successfully!' }, status: :created
+    else
+      render json: @user.errors.messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:nickname,
+                  :email,
+                  :password,
+                  :password_confirmation)
+  end
+end
