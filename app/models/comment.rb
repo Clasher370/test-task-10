4776444b2fd889e6b1f95be3f3c1665cecfa27f2
Clasher370 <tkasha89@gmail.com>
2017-11-02 19:05:08 +0300

    set published_at to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
+  include TimeSetter
+
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  before_create :set_published_at
 
   validates_presence_of :body
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,4 +4,12 @@ describe Comment do
   it { should belong_to(:author).class_name('User') }
   it { should belong_to(:post) }
   it { should validate_presence_of(:body) }
+
+  context '#set_published_at' do
+    let(:comment) { create(:comment, published_at: nil) }
+
+    it 'set published_at attr' do
+      expect(comment.published_at).not_to be_nil
+    end
+  end
 end
